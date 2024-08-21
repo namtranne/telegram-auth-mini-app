7dac643e1e539f5d4cb4acbbc23fca2a6edc9eea
Tran Hai Nam <steven.hai.ta856@gmail.com>
2024-08-21 11:44:40 +0700

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 
     useEffect(() => {
         if (WebApp.initDataUnsafe.user) {
-            setUserData(WebApp.initDataUnsafe.user as UserData)
+            setUserData(WebApp.initData as any)
         }
     }, [])
 
@@ -27,14 +27,7 @@ export default function Home() {
             {userData ? (
                 <>
                     <h1 className='text-2xl font-bold mb-4'>User Data</h1>
-                    <ul>
-                        <li>ID: {userData.id}</li>
-                        <li>First Name: {userData.first_name}</li>
-                        <li>Last Name: {userData.last_name || "N/A"}</li>
-                        <li>Username: {userData.username || "N/A"}</li>
-                        <li>Language Code: {userData.language_code}</li>
-                        <li>Is Premium: {userData.is_premium ? "Yes" : "No"}</li>
-                    </ul>
+                    {JSON.stringify(userData)}
                 </>
             ) : (
                 <div>Loading...</div>
