96d5ac050ecdb0e78de604000e0d1dbeb0be620e
Tran Hai Nam <steven.hai.ta856@gmail.com>
2024-08-21 11:33:32 +0700

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import Script from "next/script"
-import "./globals.css"
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Telegram mini app",
-    description: "Telegram mini app auth test"
-}
+  title: 'Telegram Mini App',
+  description: 'A simple Telegram Mini App using Next.js 14'
+};
 
 export default function RootLayout({
-    children
+  children,
 }: Readonly<{
-    children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang='en'>
-            <head>
-                <Script src='https://telegram.org/js/telegram-web-app.js' strategy='beforeInteractive' />
-            </head>
-            <body className={inter.className}>{children}</body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <head>
+        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
